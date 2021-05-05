2140ae4ac1138290c2d35be0bf5829fbec6b269e
Peter <peterabsolon@yahoo.com>
2021-05-05 18:25:29 +0200

    comments

--- src/react-final-form/EYInputField.js
+++ src/react-final-form/EYInputField.js
@@ -1,3 +1,7 @@
+/**
+ * Wrapepr that connects our custom EYInput with React-final-form
+ */
+
 import { EYInput } from '../components/EYInput'
 
 export const EYInputField = ({ input, meta, ...rest }) => {
